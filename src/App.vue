cbb58a3012d2f70c6788c400ea86efb215e21dc9
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-11 15:09:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import HeaderApp from './HeaderApp.vue';
+  import MainApp from './MainApp.vue';
+
+  export default {
+    components: {
+      HeaderApp,
+      MainApp
+    }
+  }
+</script>
+
+<template>
+
+  <HeaderApp/>
+
+  <MainApp/>
+
+</template>
+
+<style lang="scss">
+
+</style>
